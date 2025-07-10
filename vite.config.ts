d42da2784cb420552ee70fcac2ed779cd9884d14
Fernando Piacezzi <fernandopiacezzi@gmail.com>
2025-07-10 10:37:02 -0300

    gh pages config deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/cotacao-magica-rapida',
   server: {
     host: "::",
     port: 8080,
